85267734abe65fd17dd44bd1e3ef0f39531f964e
ChicheT <chichets@gatech.edu>
2022-04-05 23:58:28 -0400

    update launcher

--- src/views/Launcher.js
+++ src/views/Launcher.js
@@ -14,10 +14,10 @@ export default class Launcher extends React.Component {
         // SMART Authentication
         SMART.authorize({
             // clientId: Obtained from SMART-on-FHIR authenticator after registration (e.g. Cerner)
-            clientId: "",
+            clientId: "9be0555e-198a-4df3-b9cb-c5d77a387cd7",
             scope: "launch profile fhirUser openid online_access user/Patient.read user/Observation.read",
             // redirectUri: Endpoint for your application frontend
-            redirectUri: "http://0.0.0.0:3000/",
+            redirectUri: "https://chichet.github.io/SMART-on-FHIR-Growth-Chart/",
             completeInTarget: true,
             // TESTING: Define fhirServiceUrl to bypass authentication
             fhirServiceUrl: "http://hapi.fhir.org/baseR4",
